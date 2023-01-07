2117acbcf4f415b0cb9b47da322aded33ad0dc16
LonelyChimo <t2.handtke@gmail.com>
2023-01-07 21:45:57 +0100

    Create RickRoll.js

---
+++ PureHIDScripts/RickRoll.js
@@ -0,0 +1,16 @@
+//Layout and Stuff
+layout("DE");
+console.log("Never");
+typingSpeed(0,0);
+delay(800);
+
+//Pressing WIN + R and typing in the famous link
+console.log("Gonna");
+press("WIN R");
+delay(500);
+console.log("Give You");
+type("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+console.log("Up!");
+press("ENTER");
+
+//Enjoy
