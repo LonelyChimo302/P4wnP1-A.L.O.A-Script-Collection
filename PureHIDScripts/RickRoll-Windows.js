eed9dd125403641b345b2579cf27148a2cae60a3
LonelyChimo <t2.handtke@gmail.com>
2023-01-07 23:18:32 +0100

    Update RickRoll-Windows.js

--- PureHIDScripts/RickRoll-Windows.js
+++ PureHIDScripts/RickRoll-Windows.js
@@ -6,7 +6,7 @@ delay(800);
 
 //Pressing WIN + R and typing in the famous link
 console.log("Gonna");
-press("WIN R");
+press("WIN R"); //To make this work on OSX just replace this line with *press("WIN SPACE");*
 delay(500);
 console.log("Give You");
 type("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
